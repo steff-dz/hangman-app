6b6dd8ae02185f570b60748f40daa17fbb253e71
Stefanie Dziadkowiec <stefaniemarie.dz@gmail.com>
2021-04-17 10:04:36 +0200

    moved the SVG figure into it's own file

--- screens/HangmanScreen.js
+++ screens/HangmanScreen.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import { View, Modal, Text, TextInput, TouchableOpacity, Alert } from 'react-native'
-import Svg, { Circle, Rect, Line, G } from 'react-native-svg'
+import Svg, { Circle, Rect, Line } from 'react-native-svg'
 import { styles } from '../theme/stylesTheme'
 import { Alphabet } from '../utils/alphabet'
+import SvgFigure from '../components/SvgFigure'
 
 const HangmanScreen = () => {
   const [gameOver, setGameOver] = useState(true)
@@ -40,45 +41,8 @@ const HangmanScreen = () => {
     if (playerWon) {
       return newGameHandler()
     } else {
-      return showImage()
+      return <SvgFigure errors={errors} />
     }
-  }
-
-  //function to render hangman SVG char---------------------------
-  function showImage() {
-    return (
-      <View style={styles.figureContainer}>
-        <Svg height="250" width="200">
-          <Line stroke="#ff8906" x1="100" y1="0" x2="100" y2="30" strokeWidth="5" />
-          {errors > 0 && (
-            <Circle cx="100" cy="50" r="25" stroke="#e53170" strokeWidth="2.5" fill="#e53170" />
-          )}
-          {errors > 1 && (
-            <Rect
-              x="98"
-              y="75"
-              width="5"
-              height="80"
-              stroke="#e53170"
-              strokeWidth="2"
-              fill="#e53170"
-            />
-          )}
-          {errors > 2 && (
-            <Line x1="40" y1="130" x2="100" y2="85" stroke="#e53170" strokeWidth="2" />
-          )}
-          {errors > 3 && (
-            <Line x1="160" y1="130" x2="100" y2="85" stroke="#e53170" strokeWidth="2" />
-          )}
-          {errors > 4 && (
-            <Line x1="50" y1="200" x2="100" y2="150" stroke="#e53170" strokeWidth="2" />
-          )}
-          {errors > 5 && (
-            <Line x1="150" y1="200" x2="100" y2="150" stroke="#e53170" strokeWidth="2" />
-          )}
-        </Svg>
-      </View>
-    )
   }
 
   //function to handle word submit------------------------------
